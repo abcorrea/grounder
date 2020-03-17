86a6bb0f28dfb5cec468c1622c6465eb74757d36
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2020-03-17 18:42:50 +0100

    Build Release version explicitly by default

--- build.py
+++ build.py
@@ -10,7 +10,7 @@ SRC_DIR = os.path.join(PROJECT_ROOT, 'src', 'grounder')
 
 def build():
     os.makedirs(BUILD_DIR, exist_ok=True)
-    subprocess.check_call(['cmake', SRC_DIR], cwd=BUILD_DIR)
+    subprocess.check_call(['cmake', SRC_DIR, '-DCMAKE_BUILD_TYPE=Release'], cwd=BUILD_DIR)
     subprocess.check_call(['make', '-j5'], cwd=BUILD_DIR)
 
 
